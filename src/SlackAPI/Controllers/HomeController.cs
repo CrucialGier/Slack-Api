b280e1ce114adf004d855aea93002875c0ecdd8d
Shradha Pulla and Stewart Cole <shradha+stewart@>
2016-09-28 09:38:42 -0700

    Create basic files

---
+++ src/SlackAPI/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+//using SlackAPI.Models;
+//using SlackAPI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SlackAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
